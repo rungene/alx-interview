e651555d37a19866e9370b3d444719dac86a1018
rungene <rungenesamuel@gmail.com>
2023-08-19 10:33:58 -0700

    Checking for keyboard interupt, reading the line and printing to the screen

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -16,12 +16,36 @@ def process_stdin(line):
     match_input = re.match(pattern_match, line)
 
     if match_input:
-        ip_address = match.group(1)
-        str_date = match.group(2)
-        status_code = int(match.group(3))
-        file_size = int(match.group(4))
+        ip_address = match_input.group(1)
+        str_date = match_input.group(2)
+        status_code = int(match_input.group(3))
+        file_size = int(match_input.group(4))
 
         return ip_address, str_date, status_code, file_size
 
     else:
         return None
+
+
+def process_data():
+    """Process data from process_stdin"""
+    try:
+        while True:
+            # Read input from stdin
+            input_line = input()
+            # process input
+            data = process_stdin(input_line)
+            if data:
+                ip_address, str_date, status_code, file_size = data
+                print('Ip Address {}'.format(ip_address))
+                print('Date String {}'.format(str_date))
+                print('Status Code {}'.format(status_code))
+                print('File size {}'.format(file_size))
+
+    except KeyboardInterrupt:
+        print('There was a keyboard interupt')
+        sys.exit()
+
+
+if __name__ == '__main__':
+    process_data()
