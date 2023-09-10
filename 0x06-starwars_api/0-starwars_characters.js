cdd88024264e04960ffcb69b1512100c5f4634d3
rungene <rungenesamuel@gmail.com>
2023-09-10 10:35:14 -0700

    Using promises

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -6,7 +6,7 @@ const args = process.argv;
 if (args.length > 2) {
   const movieId = args[2];
 
-  const baseUrl = 'https://swapi.dev/api/people/';
+  const baseUrl = 'https://swapi.dev/api/films/';
 
   const apiUrl = baseUrl + movieId + '/';
 
@@ -19,11 +19,37 @@ if (args.length > 2) {
     } 
     else {
       const data = JSON.parse(body);
+      const characterList = data.characters
 
-      console.log('Name', data.name);
-      console.log('Height', data.height);
-    }
-  });
+      const characterRequests = characterList.map(characterUrl => {
+        return new Promise((resolve, reject) => {
+          request(characterUrl, (error, response, body) => {
+            if (error) {
+              console.error('Error', error);
+              reject(error);
+            }
+            else if (response.statusCode !== 200) {
+              console.error('Status code', response.statusCode);
+              reject(new Error(`Failed to fetch character: ${characterURL}`));
+            }
+            else {
+              const characterData = JSON.parse(body);
+              console.log(characterData.name);
+              resolve(characterData)
+          }
+          });
+        });
+      });
+      // Wait all chracter request to complete
+      Promise.all(characterRequests)
+        .then(() => {
+          console.log('All characters fetched successfully');
+        })
+        .catch(err => {
+          console.error('Error:', err);
+        });
+      }
+    });
 } else {
   console.log('Please provide id');
 }
